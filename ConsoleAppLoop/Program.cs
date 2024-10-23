b2d60bde5e95a84b367727e8d0321eb281a5b9da
Minh3782 <tle276@my.centennialcollege.ca>
2024-10-23 18:16:25 -0400

    Update Program.cs

--- ConsoleAppLoop/Program.cs
+++ ConsoleAppLoop/Program.cs
@@ -1,2 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+//loop
+//char myLetter = 'a';
+//while (myLetter <= 'm') // mandatory to have condition, no operator
+//{
+//    Console.Write(myLetter);
+//    myLetter++;
+//}
+
+//for (char i = 'a'; i < 'm'; i++)
+//{
+//    Console.WriteLine(i);
+//}
+
+//nested loop - create a square
+
+Console.Write("Enter length: ");
+int length= Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter width: ");
+int width = Convert.ToInt32(Console.ReadLine());
+Console.Write("What symbol: ");
+char symbol = Convert.ToChar(Console.ReadLine());
+
+for (int i = 0; i < length; i++)
+{
+    for (int j = 0; j < width; j++)
+    {
+        Console.Write(".");
+    }
+    Console.WriteLine(symbol);
+}
