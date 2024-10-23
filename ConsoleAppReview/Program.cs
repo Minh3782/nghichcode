5a3f71f0bd668735e27c4f8d8e8982b539009195
Minh3782 <tle276@my.centennialcollege.ca>
2024-10-23 16:43:26 -0400

    First commit_Day 4

---
+++ ConsoleAppReview/Program.cs
@@ -0,0 +1,62 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //constant and GetType method
+            const string name = "Manila";
+            Console.WriteLine(name);
+            Console.WriteLine(name.GetType());
+
+            ////Random number
+            //Console.WriteLine("Random number excercise");
+            //Random rand = new Random();
+            //int number = rand.Next(1, 32);
+            //Console.WriteLine(number);
+            //Console.WriteLine(++number);
+            //Random rand2 = new Random();
+            //double number2 = rand2.NextDouble();
+            //if(number2 >0.5)
+            //{
+            //    Console.WriteLine(Math.Ceiling(number2));
+            //}
+            //else
+            //        {
+            //    Console.WriteLine(Math.Floor(number2));
+            //}
+
+            //Basic Math operations
+            Random mathran = new Random();
+            int powerresult = mathran.Next(-9, 12);
+            //powerresult = Math.Abs(powerresult);
+            //Console.WriteLine(Math.Pow(powerresult, 3));
+            //Console.WriteLine(Math.Sqrt(powerresult));
+
+            //Random power = new Random(23);
+            //int equalresult = power.Next(-9, 10);
+            //equalresult = Math.Abs(equalresult);
+            //Console.WriteLine(Math.Pow(equalresult, 2));
+            //Console.WriteLine( Math.Sqrt(equalresult));
+            if (powerresult < 0)
+            {
+                powerresult = Math.Abs(powerresult);
+                Console.WriteLine(Math.Pow(powerresult, 3));
+            }
+            else
+            {
+                Console.WriteLine(Math.Sqrt(powerresult));
+            }
+
+            /*There are other operation:
+             *  round, ceiling, floor
+             *  min, max*/
+            
+            //Length method
+            string result = Convert.ToString(powerresult);
+            Console.WriteLine(result.Length);
+
+        }
+    }
+}
